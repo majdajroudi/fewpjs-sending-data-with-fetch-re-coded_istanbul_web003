2fc9b62464f90c6347b7efbee7de7ba578dd5fc6
Majd Ajroudi <majd.ajroudi@gmail.com>
2020-11-01 08:46:48 +0100

    commit tree 87a85f139257e9f93b7ccefd961cfeac470d6eb8

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Add your code here
+const body= document.querySelector('#body');
+const tag = document.createElement("p")
+const text = document.createTextNode("Text")
+
+
+function submitData(userName,userEmail){
+    return fetch("http://localhost:3000/users",{
+        method:"POST",
+        url: "http://localhost:3000/users",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    })
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(object){
+        console.log(object)
+        const objectId = document.createElement("p")
+        const name = document.createElement("p")
+        name.innerText = object.name
+        objectId.innerText = object.id
+        body.append(objectId);
+        body.appendChild(name)
+        body.innerHTML += `<p>${object.id} </p>`
+    })
+    .catch(function(error){
+        console.log(error.message);
+        const errorMessage = document.createElement("p")
+        const errorElement = document.createElement("p")
+        console.log(errorElement)
+        errorMessage.innerText = error.message
+        errorElement.innerText = error
+        body.appendChild(errorMessage)
+        body.appendChild(errorElement)
+        body.innerHTML += `<p>${error.message} </p>`
+    })
+}
+
+window.addEventListener("load", function(e){
+    // submitData("majd","majd@email.com")
+})
